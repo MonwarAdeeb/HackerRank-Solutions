650d0875524956f9bad3ce87134058e77d8e8afb
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-08-26 12:08:48 +0600

    Improved a Solution

--- Day 15 Linked List.py
+++ Day 15 Linked List.py
@@ -11,8 +11,16 @@ class Solution:
 
     def insert(self,head,data): 
     #Complete this method
-        print(data, end=" ")
-
+        if head is None:
+            head = Node(data)
+            self.tail = head
+        else: 
+            node = Node(data)
+            self.tail.next = node
+            self.tail = node
+        return head
+    
+    
 mylist= Solution()
 T=int(input())
 head=None
