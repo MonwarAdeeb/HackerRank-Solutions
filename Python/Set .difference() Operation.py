b0296e33fb9bb70b0d383c747396af73425e5e3f
Monwar Adeeb <monwar_adeeb@yahoo.com>
2020-10-12 10:14:06 +0600

    Added New Solution

---
+++ Python/Set .difference() Operation.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+x = input()
+a = set(map(int, input().split()))
+y = input()
+b = set(map(int, input().split()))
+
+print(len(a.difference(b)))
