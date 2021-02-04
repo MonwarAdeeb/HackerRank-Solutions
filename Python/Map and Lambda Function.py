c8a82c07a9512be0028b388b388fa2a587704bc7
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-04 13:52:24 +0600

    Added New Solution

---
+++ Python/Map and Lambda Function.py
@@ -0,0 +1,15 @@
+def cube(x): return pow(x, 3)  # complete the lambda function
+
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+
+    fibo = [0, 1]
+
+    for i in range(2, n):
+        fibo.append(fibo[i-2] + fibo[i-1])
+
+    return(fibo)
+
+
+if __name__ == '__main__':
