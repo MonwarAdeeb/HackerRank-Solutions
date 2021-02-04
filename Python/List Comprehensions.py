da08bda4a932a93217e290ae69a3cfe8ba0e8148
Monwar Adeeb <monwar_adeeb@yahoo.com>
2021-02-04 12:12:54 +0600

    Added New Solution

--- Python/List Comprehensions.py
+++ Python/List Comprehensions.py
@@ -1,7 +1,10 @@
 if __name__ == '__main__':
-    x = int(raw_input())
-    y = int(raw_input())
-    z = int(raw_input())
-    n = int(raw_input())
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
 
-    print([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if a+b+c != n])
+    print([[a, b, c] for a in range(x+1)
+           for b in range(y+1)
+           for c in range(z+1)
+           if a + b + c != n])
